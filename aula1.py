3f02e74cd5184cf154a220899f41e8a5ae571104
Aluno SENAI <Aluno@SN-345381.edu.fiesc.com.br>
2025-10-02 16:48:14 -0300

    conteudo sobre print e variaveis

---
+++ aula1.py
@@ -0,0 +1,7 @@
+# Como nosso primeiro código, vamos escapar da maldição
+print ("Olá Mundo")
+print ("lógica", "de", "programação", sep=" ",end="\n")
+print("vai", "imprimir", "em", "baixo",sep="\n")
+print("vai",end=" ")
+print("imprimir",end=" ")
+print("em",end=" ")
